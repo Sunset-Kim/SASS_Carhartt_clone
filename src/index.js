7dc7540b326d9592e9b08a3b21f8efb6811a5333
Sunset-Kim <wood2002@naver.com>
2021-05-28 22:27:42 +0900

    🎨 유틸메뉴 동작구현

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-import 'reset-css'
-import './css/style.css'
+import 'reset-css';
+import './css/style.css';
 
 import './js/scroll';
-
-console.log('sdf')
+import './js/sidebar';
--- src/js/sidebar.js
+++ src/js/sidebar.js
@@ -1,1 +1,31 @@
-(function () {})();
+(function () {
+  // utilmenu 설정하기
+  const utilMenu = document.querySelectorAll('.util-menu');
+  utilMenu.forEach(menu => {
+    menu.addEventListener('click', activeUtilMenu);
+  });
+  function activeUtilMenu(e) {
+    if (e.target.nodeName === 'SPAN') {
+      //초기화
+
+      //
+      const span = e.target;
+      const sub = span.nextElementSibling;
+
+      if (sub.classList.contains('active')) {
+        sub.classList.remove('active');
+        this.classList.remove('active');
+      } else {
+        // 초기화
+        const li = [].slice.call(this.children[0].children);
+        li.forEach(li => li.children[1].classList.remove('active'));
+
+        // 선택한 섹션 열어줌
+        this.classList.add('active');
+        sub.classList.add('active');
+      }
+    }
+  }
+
+  console.log(utilMenu);
+})();
